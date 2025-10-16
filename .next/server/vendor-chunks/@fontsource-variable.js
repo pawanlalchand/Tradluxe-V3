627effbfefa74b8f330c598a3e731e3757ce04d0
Pawan Lalchand <pawan103@gmail.com>
2025-10-16 12:22:40 +0800

    Updated trace

---
+++ .next/server/vendor-chunks/@fontsource-variable.js
@@ -0,0 +1,35 @@
+"use strict";
+/*
+ * ATTENTION: An "eval-source-map" devtool has been used.
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file with attached SourceMaps in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+exports.id = "vendor-chunks/@fontsource-variable";
+exports.ids = ["vendor-chunks/@fontsource-variable"];
+exports.modules = {
+
+/***/ "(rsc)/./node_modules/@fontsource-variable/open-sans/index.css":
+/*!***************************************************************!*\
+  !*** ./node_modules/@fontsource-variable/open-sans/index.css ***!
+  \***************************************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (__WEBPACK_DEFAULT_EXPORT__)\n/* harmony export */ });\n/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (\"c9bf80c1f385\");\nif (false) {}\n//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiKHJzYykvLi9ub2RlX21vZHVsZXMvQGZvbnRzb3VyY2UtdmFyaWFibGUvb3Blbi1zYW5zL2luZGV4LmNzcyIsIm1hcHBpbmdzIjoiOzs7O0FBQUEsaUVBQWUsY0FBYztBQUM3QixJQUFJLEtBQVUsRUFBRSxFQUF1QiIsInNvdXJjZXMiOlsid2VicGFjazovL3RyYWRsdXhlLy4vbm9kZV9tb2R1bGVzL0Bmb250c291cmNlLXZhcmlhYmxlL29wZW4tc2Fucy9pbmRleC5jc3M/N2QzMiJdLCJzb3VyY2VzQ29udGVudCI6WyJleHBvcnQgZGVmYXVsdCBcImM5YmY4MGMxZjM4NVwiXG5pZiAobW9kdWxlLmhvdCkgeyBtb2R1bGUuaG90LmFjY2VwdCgpIH1cbiJdLCJuYW1lcyI6W10sInNvdXJjZVJvb3QiOiIifQ==\n//# sourceURL=webpack-internal:///(rsc)/./node_modules/@fontsource-variable/open-sans/index.css\n");
+
+/***/ }),
+
+/***/ "(rsc)/./node_modules/@fontsource-variable/playfair-display/index.css":
+/*!**********************************************************************!*\
+  !*** ./node_modules/@fontsource-variable/playfair-display/index.css ***!
+  \**********************************************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (__WEBPACK_DEFAULT_EXPORT__)\n/* harmony export */ });\n/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (\"9f69c52afd9f\");\nif (false) {}\n//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiKHJzYykvLi9ub2RlX21vZHVsZXMvQGZvbnRzb3VyY2UtdmFyaWFibGUvcGxheWZhaXItZGlzcGxheS9pbmRleC5jc3MiLCJtYXBwaW5ncyI6Ijs7OztBQUFBLGlFQUFlLGNBQWM7QUFDN0IsSUFBSSxLQUFVLEVBQUUsRUFBdUIiLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly90cmFkbHV4ZS8uL25vZGVfbW9kdWxlcy9AZm9udHNvdXJjZS12YXJpYWJsZS9wbGF5ZmFpci1kaXNwbGF5L2luZGV4LmNzcz9mNmQ0Il0sInNvdXJjZXNDb250ZW50IjpbImV4cG9ydCBkZWZhdWx0IFwiOWY2OWM1MmFmZDlmXCJcbmlmIChtb2R1bGUuaG90KSB7IG1vZHVsZS5ob3QuYWNjZXB0KCkgfVxuIl0sIm5hbWVzIjpbXSwic291cmNlUm9vdCI6IiJ9\n//# sourceURL=webpack-internal:///(rsc)/./node_modules/@fontsource-variable/playfair-display/index.css\n");
+
+/***/ })
+
+};
+;
